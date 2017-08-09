bbdf739aa038a5f8e5b325dd7d6664662790997e
Calley O'Neil <calleyoneil@gmail.com>
2017-08-09 11:27:50 -0500

    Update keys.js

--- keys.js
+++ keys.js
@@ -4,8 +4,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-    consumer_key: 'y07AkKukmzoa9P45K1a9A0mp2',
-    consumer_secret: 'ZDHY1YOAIdkcSFTYpe5mvURJ9oc3z55hQ7Bwh132R5zJtiVxH8',
-    access_token_key: '870903505-i9OaAcyH3Pzcvg48Ym9kUAwQDNbwQoPppUpVwYWH',
-    access_token_secret: 'KLKLekNfDDewmCFGtVt9QgVX2U9CqHMoYOH0uJoGTEtIm'
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
 }
